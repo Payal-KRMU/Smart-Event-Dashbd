e45ec0d2291c6d47df83801af8ef327d24d1df4e
Payal-KRMU <payalkushwaha230506@gmail.com>
2026-02-08 18:44:34 +0530

    Added DOM selectors

---
+++ script.js
@@ -0,0 +1,5 @@
+const titleInput = document.getElementById("title");
+const dateInput = document.getElementById("date");
+const categoryInput = document.getElementById("category");
+const descInput = document.getElementById("desc");
+const eventsDiv = document.getElementById("events");
