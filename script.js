f215db6ff073fc9883e480ad659f02a3629abc01
Payal-KRMU <payalkushwaha230506@gmail.com>
2026-02-09 09:50:20 +0530

    Implemented add event logic

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const dateInput = document.getElementById("date");
 const categoryInput = document.getElementById("category");
 const descInput = document.getElementById("desc");
 const eventsDiv = document.getElementById("events");
+document.getElementById("addBtn").addEventListener("click", addEvent);
+
+document.getElementById("clearBtn").addEventListener("click", () => {
+  eventsDiv.innerHTML = "";
+});
+document.getElementById("sampleBtn").addEventListener("click", addSample);
