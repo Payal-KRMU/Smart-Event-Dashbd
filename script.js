6a365f22251288b348eb3ba253e89137f5c5323f
Payal-KRMU <payalkushwaha230506@gmail.com>
2026-02-09 09:51:27 +0530

    Added sample event feature

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ document.getElementById("clearBtn").addEventListener("click", () => {
   eventsDiv.innerHTML = "";
 });
 document.getElementById("sampleBtn").addEventListener("click", addSample);
+
+function addEvent() {
+  if (!titleInput.value) return alert("Enter title");
+
+  const div = document.createElement("div");
+  div.className = "event";
+
+  div.innerHTML = `
+    <h3>${titleInput.value}</h3>
+    <p>📅 ${dateInput.value}</p>
+    <span>${categoryInput.value}</span>
+    <p>${descInput.value}</p>
+    <button class="delete">❌</button>
+  `;
+
+  eventsDiv.appendChild(div);
+  clearForm();
+}
